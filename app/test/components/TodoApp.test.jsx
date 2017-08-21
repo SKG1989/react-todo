11d6288b0174c8e4dacf3171cb9de339aeeec5b1
santosh kumar gaddam <santosh.gaddam@united.com>
2017-08-21 15:45:31 -0500

    Complete handleAddTodo

--- app/test/components/TodoApp.test.jsx
+++ app/test/components/TodoApp.test.jsx
@@ -10,4 +10,17 @@ describe('TodoApp', () => {
     it('should exist', () => {
         expect(TodoApp).toExist();
     });
+
+    it('shoudl add todo to the todos state on handleAddTodo', () => {
+       var todoText = 'Test Test';
+       var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+       todoApp.setState({
+          todos: []
+       });
+       todoApp.handleAddtodo(todoText);
+
+        expect(todoApp.state.todos[0].text).toBe(todoText);
+
+    });
 });
