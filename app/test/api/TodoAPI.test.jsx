3e722ea2e993b26c9637f4f004896075a85516c4
santosh kumar gaddam <santosh.gaddam@united.com>
2017-08-21 18:51:47 -0500

    Added searchText Filter

--- app/test/api/TodoAPI.test.jsx
+++ app/test/api/TodoAPI.test.jsx
@@ -58,4 +58,46 @@ describe('TodoAPI', () => {
             expect(actualTodos).toEqual(todos);
         });
     });
+
+    describe('filterTodos', () => {
+       var todos = [{
+           id: 1,
+           text: 'Some Text here',
+           completed: true
+       },{
+           id: 2,
+           text: 'Other Text here',
+           completed: false
+       },{
+           id: 3,
+           text: 'Some Text here',
+           completed: true
+       }];
+
+       it('should return all items if showCompleted is true', () => {
+          var filterdTodos = TodoAPI.filterTodos(todos, true, '');
+          expect(filterdTodos.length).toBe(3);
+       });
+
+        it('should return non completed todos items if showCompleted is false', () => {
+            var filterdTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filterdTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filterdTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filterdTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by searchText', () => {
+            var filterdTodos = TodoAPI.filterTodos(todos, true, 'some');
+            expect(filterdTodos.length).toBe(2);
+        });
+
+        it('should return all items if searchText is empty', () => {
+            var filterdTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filterdTodos.length).toBe(3);
+        });
+
+    });
 });
